17e7b4c2c99081a68d06dfc4b4a32aa7258db8e3
skyangel <skyangel@iu.edu>
2024-11-15 22:24:59 -0500

    added requirements.txt

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=['http://localhost:3000', 'https://g4ntt.vercel.app/'])
 
 @app.route('/api/home', methods=['GET'])
 def return_home():
