c2616eded548f0de2ac284ecf60f390716de7efe
skyangel <skyangel@iu.edu>
2024-11-15 22:38:59 -0500

    bug fixes

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function Header() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   const [flyer, setFlyer] = React.useState(false);
-  const [flyerTwo, setFlyerTwo] = React.useState(false);
+  // const [flyerTwo, setFlyerTwo] = React.useState(false);
 
   return (
     <header className='fixed top-0 w-full clearNav z-50'>
       <div className='max-w-5xl mx-auto flex flex-wrap p-5 flex-col md:flex-row'>
         <div className='flex flex-row items-center justify-between p-3 md:p-1'>
-          <a
+          <Link
             href='/'
             className='flex text-3xl text-white font-medium mb-4 md:mb-0'
           >
             GY4NTT
-          </a>
+          </Link>
           <button
             className='text-white pb-4 cursor-pointer leading-none px-3 py-1 md:hidden outline-none focus:outline-none content-end ml-auto'
             type='button'
@@ -46,16 +47,19 @@ export default function Header() {
           }
         >
           <div className='md:ml-auto md:mr-auto font-4 pt-1 md:pl-14 pl-1 flex flex-wrap items-center md:text-base text-1xl md:justify-center justify-items-start'>
-            <a className='mr-11 pr-2 cursor-pointer text-gray-300 hover:text-white font-semibold tr04'>
+            <Link
+              href='/features'
+              className='mr-11 pr-2 cursor-pointer text-gray-300 hover:text-white font-semibold tr04'
+            >
               Features
-            </a>
+            </Link>
             <div className='relative'>
               <button
                 type='button'
                 className="
                    group rounded-md text-gray-300 inline-flex items-center text-base font-medium focus:outline-none pb-8'
                   "
-                onMouseEnter={() => (setFlyer(!flyer), setFlyerTwo(false))}
+                // onMouseEnter={() => (setFlyer(!flyer), setFlyerTwo(false))}
               >
                 <span className='tr04'>Templates</span>
                 <svg
@@ -86,8 +90,8 @@ export default function Header() {
               >
                 <div className='rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden'>
                   <div className='relative grid gap-6 bg-black px-2 py-6 sm:gap-8 '>
-                    <a
-                      href='/'
+                    <Link
+                      href='/template-1'
                       className='-m-3 p-3 flex items-start rounded-lg hover:bg-gray-800 tr04'
                     >
                       <div className='ml-4'>
@@ -98,9 +102,9 @@ export default function Header() {
                           First Template
                         </p>
                       </div>
-                    </a>
-                    <a
-                      href='/'
+                    </Link>
+                    <Link
+                      href='/template-2'
                       className='-m-3 p-3 flex items-start rounded-lg hover:bg-gray-800 tr04'
                     >
                       <div className='ml-4'>
@@ -111,9 +115,9 @@ export default function Header() {
                           Second Template
                         </p>
                       </div>
-                    </a>
-                    <a
-                      href='/'
+                    </Link>
+                    <Link
+                      href='/template-3'
                       className='-m-3 p-3 flex items-start rounded-lg hover:bg-gray-800 tr04'
                     >
                       <div className='ml-4'>
@@ -124,17 +128,23 @@ export default function Header() {
                           Third Template
                         </p>
                       </div>
-                    </a>
+                    </Link>
                   </div>
                 </div>
               </div>
             </div>
-            <a className='mr-12 md:ml-11 ml-0 cursor-pointer text-gray-300 hover:text-white font-semibold tr04'>
+            <Link
+              href='/pricing'
+              className='mr-12 md:ml-11 ml-0 cursor-pointer text-gray-300 hover:text-white font-semibold tr04'
+            >
               Pricing
-            </a>
-            <a className='mr-5 cursor-pointer text-gray-300 hover:text-white font-semibold tr04'>
+            </Link>
+            <Link
+              href='/careers'
+              className='mr-5 cursor-pointer text-gray-300 hover:text-white font-semibold tr04'
+            >
               Careers
-            </a>
+            </Link>
           </div>
           <a
             href='https://twitter.com/'
@@ -143,7 +153,7 @@ export default function Header() {
             className='invisible md:visible'
           >
             <svg
-              title='Twitter'
+              // title='Twitter'
               width='30'
               height='17'
               viewBox='0 0 50 40'
@@ -171,13 +181,13 @@ export default function Header() {
               viewBox='0 0 25 24'
               fill='none'
               xmlns='http://www.w3.org/2000/svg'
-              title='GitHub logo'
+              // title='GitHub logo'
               className='github-link--logo'
             >
               <path
                 data-v-54e46119=''
-                fill-rule='evenodd'
-                clip-rule='evenodd'
+                fillRule='evenodd'
+                clipRule='evenodd'
                 d='M12.3019 0C5.50526 0 0 5.50526 0 12.3019C0 17.7392 3.52669 22.3458 8.4127 23.977C9.0244 24.0902 9.25095 23.7126 9.25095 23.3804C9.25095 23.0858 9.2434 22.3156 9.23585 21.2885C5.81488 22.0286 5.08991 19.6422 5.08991 19.6422C4.53108 18.2225 3.72304 17.8373 3.72304 17.8373C2.60537 17.0746 3.80611 17.0897 3.80611 17.0897C5.03705 17.1803 5.69405 18.3584 5.69405 18.3584C6.78906 20.2388 8.57129 19.6951 9.27361 19.3779C9.38688 18.585 9.70406 18.0412 10.0514 17.7316C7.32524 17.4295 4.45556 16.3723 4.45556 11.66C4.45556 10.3158 4.93132 9.22074 5.72426 8.35984C5.59588 8.04266 5.17298 6.79662 5.83754 5.10501C5.83754 5.10501 6.87213 4.77274 9.22074 6.36616C10.2025 6.0943 11.2522 5.95837 12.3019 5.95082C13.344 5.95837 14.4013 6.0943 15.383 6.36616C17.7316 4.77274 18.7662 5.10501 18.7662 5.10501C19.4383 6.79662 19.0154 8.05021 18.887 8.35984C19.6724 9.22074 20.1482 10.3158 20.1482 11.66C20.1482 16.3874 17.271 17.422 14.5297 17.7316C14.9677 18.1092 15.3679 18.8644 15.3679 20.0123C15.3679 21.6586 15.3528 22.9801 15.3528 23.3879C15.3528 23.7202 15.5718 24.0978 16.1986 23.977C21.0846 22.3458 24.6038 17.7392 24.6038 12.3094C24.6038 5.50526 19.0985 0 12.3019 0Z'
                 fill='white'
               ></path>
